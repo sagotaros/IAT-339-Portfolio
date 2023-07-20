2e809a5f5922af36f28fdd309b79eed2ca3d9deb
sagotaro <jimmyzeng518@gmail.com>
2023-07-19 21:46:28 -0700

    added about page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,7 @@
 "use strict";
 const menuItem = document.querySelector('.nav-items');
 const menuIcon = document.querySelector('.menu-icon');
-const stars = document.querySelectorAll('.ratings-click i');
 
-//https://www.youtube.com/watch?v=q1xhbc-oKnc Watched this tutorial for the stars
-
-stars.forEach((star, index1) => {
-    star.addEventListener("click", () => {
-    stars.forEach((star, index2) => {
-        index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
-    })    
-    });
-});
 
 menuIcon.addEventListener('click',() => {
     const visiblity = menuItem.getAttribute('data-visible')
@@ -24,11 +14,3 @@ menuIcon.addEventListener('click',() => {
         menuIcon.setAttribute('aria-expanded', false)
     }
 });
-
-function reviewAlert(){
-    alert("Thank you for the review!");
-}
-
-function checkoutAlert(){
-    alert("Thank you for the purchase!");
-}
