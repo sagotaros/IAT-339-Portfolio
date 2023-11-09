234797223f5f3d6ba89a09b6590b868065b48555
sagotaro <jza255@sfu.ca>
2023-11-08 18:28:54 -0800

    added scroll animation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
 "use strict";
 const menuItem = document.querySelector('.nav-items');
 const menuIcon = document.querySelector('.menu-icon');
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    rootMargin: "0px 0px -20px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},
+appearOptions);
+
+faders.forEach(fader =>{
+    appearOnScroll.observe(fader);
+});
+
 
 
 menuIcon.addEventListener('click',() => {
